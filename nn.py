0b56d784c036242fdfeedebe0a61a01e16a490a9
Jon <joharrity@gmail.com>
2018-09-16 22:28:12 -0400

    rewrote back propagation method

--- nn.py
+++ nn.py
@@ -6,7 +6,12 @@ import pickle
 
 class Neuron:
     def to_string(self):
-        return 'weights %s, bias %s' % (self.weights, self.bias)
+        return 'neuron with weights %s' % (self.weights)
+    def __repr__(self):
+        return self.to_string()
+    def __init__(self, weights=None):
+        if weights:
+            self.weights = weights
 
 
 class NeuralNetwork:
@@ -19,23 +24,60 @@ class NeuralNetwork:
     #only call after feed_forward, get array output of ANN
     def get_output(self):
         return [n.output for n in self.layers[-1]]
+    def __str__(self):
+        return 'neural network with layers %s' % str([len(self.layers[0][0].weights)]+[len(n) for n in self.layers])
+    def __repr__(self):
+        return self.__str__()
         
 
     def feed_forward(self, inputs):
-        for layer in self.layers:
+        for i in range(len(self.layers)):
+            layer = self.layers[i]
             next_inputs = []
             for neuron in layer:
-                activation = neuron.bias
+                #self.biases[i] (layer bias) > neuron.bias (neuron bias) > 0 (disable)
+                activation = -neuron.bias
                 for x in range(len(inputs)):
                     if x >= len(neuron.weights):
-                        raise 'invalid input: %s inputs supplied, but only %s nodes in first layer' % (range(len(inputs)), len(neuron.weights))
+                        raise Exception('invalid input: %s inputs supplied, but only %s nodes in first layer' % (range(len(inputs)), len(neuron.weights)))
                     activation += inputs[x] * neuron.weights[x]
                 neuron.output = self.transfer(activation)
                 next_inputs.append(neuron.output)
 
             inputs = next_inputs
         return self.get_output()
+    
+    def back_propogate(self, expected, inputs):
+        error_sum = 0
+        
+        for i in range(len(expected)):
+            actual = self.layers[-1][i].output
+            error = expected[i] - actual
+            error_sum += pow(error, 2)
+            output_gradient = self.transfer_derivative(actual) * error
+            #loop through weights between hidden layer
+            for j in range(len(self.layers[-2])):
+                previous_output = self.layers[-2][j].output
+                delta = self.learning_rate * previous_output * output_gradient
+                self.layers[-1][i].weights[j] += delta
+                hidden_gradient = self.transfer_derivative(previous_output) * output_gradient * previous_output
+                #loop through weights between input layer
+                for k in range(len(self.layers[0][0].weights)):
+                    delta = self.learning_rate * inputs[k] * hidden_gradient
+                    self.layers[-2][j].weights[k] += delta
 
+                #update bias for hidden node
+                delta = self.learning_rate * hidden_gradient
+                self.layers[-2][j].bias -= delta
+            
+            #update bias for output node
+            delta = self.learning_rate * output_gradient
+            self.layers[-1][i].bias -= delta
+
+        return error_sum
+
+
+    """
     def back_propogate(self, expected):
         for i in range(len(expected)):#first find error of output layer
             neuron = self.layers[-1][i]
@@ -49,44 +91,67 @@ class NeuralNetwork:
                     error += next_neuron.delta * next_neuron.weights[ineuron]
                 neuron.delta = error * self.transfer_derivative(neuron.output)
    
-    """
     def transfer(self, x):
+        #print('transfer %s'%x)
         return tanh(x)
     def transfer_derivative(self, x):
         return 1-tanh(x)**2
     """
     def transfer(self, x):
-        return 1 / (1+exp(x))
+        return 1 / (1+exp(-x))
     def transfer_derivative(self, x):
-        return self.transfer(x) * (1-self.transfer(x))
-    
-    def __init__(self, layer_counts, learning_rate=0.5):#initialize network with random weights and biases
-        layers = []
+        #return x * (1 - x)
+        return self.transfer(x) * (self.transfer(-1*x))
+
+    def __init__(self, layer_counts, learning_rate=0.35):#initialize network with random weights and biases
+        self.layers = []
+        self.biases = []
         input_count = layer_counts[0]
         layer_counts = layer_counts[1:]
         for size in layer_counts:
             new_layer = []
             for i in range(size):
                 neuron = Neuron()
-                neuron.bias = 1
                 neuron.weights = [random.random() for i in range(input_count)]
+                neuron.bias = random.random()
                 new_layer.append(neuron)
-            layers.append(new_layer)
+            self.layers.append(new_layer)
+            self.biases.append(0.5)
             input_count = size
-        self.layers = layers
         self.learning_rate = learning_rate
 
     def update_weights(self, inputs):
+        layer_count = 1
         for layer in self.layers:
             next_inputs = []
+            sav_changes = []
+            count_neurons_changed = 0
+            count_neurons_unchanged = 0
             for neuron in layer:
+                total_change = 0
                 for i in range(len(inputs)):
+                    weight_start = neuron.weights[i]
                     neuron.weights[i] -= inputs[i] * neuron.delta * self.learning_rate * neuron.output
-                neuron.bias -= neuron.delta * self.learning_rate
+                    weight_end = neuron.weights[i]
+                    total_change += abs(weight_start - weight_end)
+                    if weight_start == weight_end:
+                        count_neurons_unchanged += 1
+ #                       print('unchanged neuron, weight: %s' % weight_start)
+                    else:
+                        count_neurons_changed += 1
+                        #print('change neuron, delta %s, output %s, input %s %s'%(neuron.delta, neuron.output, inputs[i], self.biases[layer_count-1]))
+
+                #self.biases[layer_count-1] (layer bias) > neuron.bias
+                self.biases[layer_count-1] -= neuron.delta * self.learning_rate
+                sav_changes.append(total_change)
+                
                 next_inputs.append(neuron.output)
+#            print('layer %s ; weight changes (%s / %s) %s' % (layer_count, count_neurons_changed, (count_neurons_changed + count_neurons_unchanged), sav_changes))
             inputs = next_inputs
+            layer_count += 1
 
     def train(self, dataset, epochs):
+        print('calling train, dataset len %s, epochs: %s' % (len(dataset), epochs))
         if type(dataset) != type([]):
             raise Exception('NeuralNetwork.train: dataset not an array')
         if len(dataset) < 1:
@@ -98,17 +163,18 @@ class NeuralNetwork:
 
         for gen in range(epochs):
             dataset.sort(key=lambda x:random.random())
+            #print('training dataset %s'%dataset)
             for training_set in dataset:
                 error = 0
                 count = 0
                 self.feed_forward(training_set['inputs'])
-                self.back_propogate(training_set['expected'])
-                self.update_weights(training_set['inputs'])
-                error += sum((self.layers[-1][i].delta)**2 for i in range(len(training_set['expected'])))
+                error += self.back_propogate(training_set['expected'], training_set['inputs'])
+                #self.update_weights(training_set['inputs'])
+                #error += sum((self.layers[-1][i].delta)**2 for i in range(len(training_set['expected'])))
                # print('output %s for expected %s' % (self.layers[-1][0].output, training_set['expected']))
         #    print('epoch %s: error is %s' % (str(gen), error))
                 count += 1
-            if gen == 1:
+            if gen == 0:
                 print('first epoch: avg. error = %s' % str(error/count))
             if gen+1 == epochs:
                 print('last epoch: avg. error = %s' % str(error/count))
@@ -149,7 +215,7 @@ def permute(data_set, first):
         for z in permute(first[0:i]+first[i+1:], first+n):
             yield z
 
-def get_training_data(board=None, current_player='x'):
+def get_all_training_data(board=None, current_player='x'):
     data = []
     turn_switch = {'x':'o','o':'x'}
 
@@ -257,6 +323,12 @@ def develop_network(epochs=500, hidden_layers=5):
     nn.train(training_data, epochs)
     save_network(nn)
 
+
+def get_xor_data():
+    return [{'inputs':n[:2],'expected':[n[2]]} for n in [[0,0,0],[0,1,1],[1,0,1],[1,1,0]]]
+def print_bias(net):
+    data = [net.layers[0][0].bias, net.layers[0][1].bias,net.layers[1][0].bias]
+    print(data)
 
 
 if __name__ == '__main__':
